67b69070c8ca43f0421f1b986cbd2976a7ae1039
alcibiade Streit-Soumaila <alcibiade.streit-soumaila@epitech.eu>
2026-05-13 09:46:48 +0200

    [+] add the placement in the arena

--- src/main.c
+++ src/main.c
@@ -20,17 +20,29 @@ int free_all(corewar_t *war)
     return SUCCESS_EXIT;
 }
 
+int placement_in_arena(corewar_t *war)
+{
+    int len = len_node(war->robot);
+    int load_address = 0;
+
+    if (len == 0)
+        return FAILURE_EXIT;
+    for (nodes_t *tmp = war->robot; tmp; tmp = tmp->next) {
+        for (int i = 0; i < ((robot_t *)tmp->data)->size; i++)
+            war->arena[(load_address + i) % MEM_SIZE]
+        = ((robot_t *)tmp->data)->code[i];
+        load_address += MEM_SIZE / len;
+    }
+    return SUCCESS_EXIT;
+}
+
 int main(int ac, char **av)
 {
     corewar_t *war = NULL;
-    robot_t *rob = NULL;
 
     if (init(&war, av) != SUCCESS_EXIT || check_flag(war, ac, av) == -1)
-        return FAILURE;
-    for (nodes_t *curr = war->robot; curr; curr = curr->next) {
-        rob = (robot_t *)curr->data;
-        printf("%d\n", rob->id);
-    }
+        return FAILURE_EXIT;
+    placement_in_arena(war);
     free_all(war);
     return SUCCESS_EXIT;
 }
