2241c4065728c9c580c08d0778846e2780fe7d1a
alcibiade Streit-Soumaila <alcibiade.streit-soumaila@epitech.eu>
2026-05-13 09:48:01 +0200

    [+] add flag -a if he's present

--- src/flags/prog_nb.c
+++ src/flags/prog_nb.c
@@ -11,9 +11,10 @@ int prog_nb(corewar_t *core, int ac, char **av, int *index)
 {
     int id = av[*index + 1] ? my_getnbr(av[*index + 1]) : -1;
 
-    if (id <= 0 || !av[*index + 2] ||
-        !create_champion(core, av[*index + 2], id))
+    if (id <= 0 || !av[*index + 2])
         return -1;
     *index += 2;
-    return 0;
+    if (my_strcmp(av[*index], "-a") == 0)
+        return prog_a(core, id, av, index);
+    return get_robot(core, av[*index], id);
 }
