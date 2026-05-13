95da53a95143a21e7c54439fdb5c8f48be29ffcf
alcibiade Streit-Soumaila <alcibiade.streit-soumaila@epitech.eu>
2026-05-13 09:45:01 +0200

    [+] add verification of multiple flag combined in correct order

--- src/flags/check_flag.c
+++ src/flags/check_flag.c
@@ -10,19 +10,25 @@
 const flag_t flag[] = {
     {"-h", print_help},
     {"-n", prog_nb},
+    {"-a", prog_a},
     {"-dump", nbr_cycle},
     {NULL, NULL}
 };
 
-int check_flag_fct(corewar_t *core, int ac, char **av, int *index)
+int check_flag_fct(corewar_t *core, int id, char **av, int *index)
 {
+    int status = 0;
+
     for (int i = 0; flag[i].name; i++) {
-        if (my_strcmp(av[*index], flag[i].name) != 0)
-            continue;
-        if (flag[i].fct(core, ac, av, index) == -1)
+        if (my_strcmp(av[*index], flag[i].name) == 0)
+            status = flag[i].fct(core, id, av, index);
+        if (status == -1)
             return -1;
-        return 1;
+        if (status != 0)
+            return status;
     }
+    if (get_robot(core, av[*index], id) != SUCCESS_EXIT)
+        return -1;
     return 0;
 }
 
@@ -32,10 +38,10 @@ int check_flag(corewar_t *core, int ac, char **av)
     int id = 1;
 
     for (int i = 1; av[i]; i++) {
-        status = check_flag_fct(core, ac, av, &i);
+        status = check_flag_fct(core, id, av, &i);
         if (status == 1)
             continue;
-        if (status == -1 || !create_champion(core, av[i], id))
+        if (status == -1)
             return -1;
         id++;
     }
