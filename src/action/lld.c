de4f5485c2972b92f930761b169a960499023b6f
alcibiade Streit-Soumaila <alcibiade.streit-soumaila@epitech.eu>
2026-05-14 23:31:49 +0200

    [+] add lld function but only prototype

---
+++ src/action/lld.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2026
+** corewar_pub
+** File description:
+** live
+*/
+
+#include "../../include/header.h"
+
+int lld(corewar_t *war, robot_t *robot, processus_t *proc)
+{
+    return SUCCESS_EXIT;
+}
