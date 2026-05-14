2a7b4b2073499c3e75da3323608a458edc45d382
alcibiade Streit-Soumaila <alcibiade.streit-soumaila@epitech.eu>
2026-05-14 23:32:00 +0200

    [+] add xor function but only prototype

---
+++ src/action/my_xor.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2026
+** corewar_pub
+** File description:
+** live
+*/
+
+#include "../../include/header.h"
+
+int my_xor(corewar_t *war, robot_t *robot, processus_t *proc)
+{
+    return SUCCESS_EXIT;
+}
