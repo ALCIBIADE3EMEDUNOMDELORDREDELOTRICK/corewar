d626da15159c43275409eb74cd4aad1024c56e0d
alcibiade Streit-Soumaila <alcibiade.streit-soumaila@epitech.eu>
2026-05-14 23:29:22 +0200

    [+] add cycle in processus

--- src/action/cycle_action.c
+++ src/action/cycle_action.c
@@ -10,34 +10,38 @@
 int detect_command(processus_t *proc, corewar_t *war, robot_t *robot)
 {
     int action[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
-    int (*func[])(corewar_t *war, robot_t *robot, processus_t *proc) = {live};
+    int (*func[])(corewar_t *war, robot_t *robot, processus_t *proc) =
+    {live, my_ld, my_st, add, sub, my_and, my_or, my_xor, zjmp, ldi,
+        sti, my_fork, lld, lldi, lfork, print};
 
-    for (int i = 0; action[i] != 0; i++) {
-        if (action[i] == war->arena[proc->pc])
+    for (int i = 0; i != 16; i++) {
+        if (action[i] == war->arena[proc->pc]) {
+            proc->pc++;
             return func[i](war, robot, proc);
+        }
     }
-    return SUCCESS_EXIT;
+    return FAILURE_EXIT;
 }
 
 int choose_action(corewar_t *war, robot_t *robot)
 {
+    int status = -1;
+
     for (processus_t *tmp = robot->processus; tmp; tmp = tmp->next) {
-        if (detect_command(tmp, war, robot) == FAILURE_EXIT)
-            return FAILURE_EXIT;
+        if (tmp->cycle > 0)
+            tmp->cycle--;
+        if (tmp->cycle == 0)
+            status = detect_command(tmp, war, robot);
+        if (status == FAILURE_EXIT)
+            return status;
     }
     return SUCCESS_EXIT;
 }
 
 int do_action(corewar_t *war)
 {
-    int status = SUCCESS_EXIT;
-
     for (nodes_t *tmp = war->robot; tmp; tmp = tmp->next) {
-        if (((robot_t *)tmp->data)->cycle != 0)
-            ((robot_t *)tmp->data)->cycle--;
-        if (((robot_t *)tmp->data)->cycle == 0)
-            status = choose_action(war, tmp->data);
-        if (status == FAILURE_EXIT)
+        if (choose_action(war, tmp->data) == FAILURE_EXIT)
             return FAILURE_EXIT;
     }
     return SUCCESS_EXIT;
