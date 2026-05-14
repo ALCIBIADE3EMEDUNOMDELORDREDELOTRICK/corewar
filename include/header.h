16f1be627b6cd24e6e83998d5db2defb52a788cd
alcibiade Streit-Soumaila <alcibiade.streit-soumaila@epitech.eu>
2026-05-14 23:28:47 +0200

    [+] add prototype of multiple function

--- include/header.h
+++ include/header.h
@@ -28,6 +28,7 @@ typedef struct processus {
     int carry;
     int reg[REG_NUMBER];
     int id;
+    int cycle;
     struct processus *next;
 } processus_t;
 
@@ -38,7 +39,6 @@ typedef struct robot {
     int size;
     unsigned char *code;
     int load;
-    int cycle;
     processus_t *processus;
 } robot_t;
 
@@ -58,7 +58,22 @@ typedef struct flag_s {
     fct_t fct;
 } flag_t;
 
+int add(corewar_t *war, robot_t *robot, processus_t *proc);
+int my_and(corewar_t *war, robot_t *robot, processus_t *proc);
 char *get_file(const char *filename);
+int zjmp(corewar_t *war, robot_t *robot, processus_t *proc);
+int sub(corewar_t *war, robot_t *robot, processus_t *proc);
+int my_st(corewar_t *war, robot_t *robot, processus_t *proc);
+int sti(corewar_t *war, robot_t *robot, processus_t *proc);
+int print(corewar_t *war, robot_t *robot, processus_t *proc);
+int my_or(corewar_t *war, robot_t *robot, processus_t *proc);
+int lldi(corewar_t *war, robot_t *robot, processus_t *proc);
+int my_xor(corewar_t *war, robot_t *robot, processus_t *proc);
+int lld(corewar_t *war, robot_t *robot, processus_t *proc);
+int my_ld(corewar_t *war, robot_t *robot, processus_t *proc);
+int lfork(corewar_t *war, robot_t *robot, processus_t *proc);
+int ldi(corewar_t *war, robot_t *robot, processus_t *proc);
+int my_fork(corewar_t *war, robot_t *robot, processus_t *proc);
 robot_t *create_champion(corewar_t *core, char *filename, int id);
 int prog_nb(corewar_t *core, int ac, char **av, int *index);
 int print_help(corewar_t *core, int ac, char **av, int *index);
@@ -68,6 +83,10 @@ int init_robot(corewar_t *war, char **av);
 int get_robot(corewar_t *war, char *path, int id);
 int free_all(corewar_t *war);
 int loop(corewar_t *war);
+int live(corewar_t *war, robot_t *robot, processus_t *proc);
+int do_action(corewar_t *war);
+void free_robot(void *data);
+int die(corewar_t *war);
 int dump(corewar_t *war);
 int print_win(corewar_t *war);
 int prog_a(corewar_t *core, int id, char **av, int *index);
