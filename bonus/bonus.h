00b790cf8add20cada9ecc3947c13f45ac999c14
Jessymgadd <jessym1.gaddacha@epitech.eu>
2026-05-15 00:52:59 +0200

    [+] add bonus header with csfml lib

---
+++ bonus/bonus.h
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2026
+** robot_factory
+** File description:
+** header
+*/
+
+
+#include <SFML/Graphics.h>
+#include <SFML/Window.h>
+#include <SFML/System.h>
+
+#ifndef BONUS_H
+    #define BONUS_H
+sfText *create_text(sfFont *font, sfColor *color);
+
+#endif
