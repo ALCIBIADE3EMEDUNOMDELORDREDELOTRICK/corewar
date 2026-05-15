c2df476e1c3facb79213a52c2a6c1899ac9a6a6f
alcibiade Streit-Soumaila <alcibiade.streit-soumaila@epitech.eu>
2026-05-15 19:14:21 +0200

    [+] add initialisation for cycle sinces check

--- src/initialisation/init.c
+++ src/initialisation/init.c
@@ -15,10 +15,11 @@ int init(corewar_t **war, char **av)
     for (int i = 0; i != MEM_SIZE; i++)
         (*war)->arena[i] = 0;
     (*war)->robot = NULL;
-    (*war)->cycle = -1;
+    (*war)->cycle = -2;
     (*war)->load = -1;
     (*war)->cycle_to_die = CYCLE_TO_DIE;
     (*war)->current_cycle = 0;
+    (*war)->cycles_since_check = 0;
     (*war)->nbr_live = 0;
     (*war)->check = 0;
     return SUCCESS_EXIT;
