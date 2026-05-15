2467f80285baa36c753b407d38a3a7751a4449b3
alcibiade Streit-Soumaila <alcibiade.streit-soumaila@epitech.eu>
2026-05-15 11:18:13 +0200

    [+] add incrementation of since check and die cycle

--- src/loop/main_loop.c
+++ src/loop/main_loop.c
@@ -12,10 +12,11 @@ int loop(corewar_t *war)
     for (int i = 0; i != war->cycle; i++) {
         if (do_action(war) == FAILURE_EXIT)
             return FAILURE_EXIT;
-        if (len_node(war->robot) <= 1)
+        if (len_node(war->robot) <= 1 || war->cycle_to_die <= 0)
             break;
         war->current_cycle++;
-        if (war->current_cycle >= war->cycle_to_die)
+        war->cycles_since_check++;
+        if (war->cycles_since_check >= war->cycle_to_die)
             die(war);
     }
     if (war->cycle != -1)
