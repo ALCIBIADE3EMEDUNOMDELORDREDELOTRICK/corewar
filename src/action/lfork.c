272cfd6e43d530e9d584fac7361d92741dffd729
Jessymgadd <jessym1.gaddacha@epitech.eu>
2026-05-15 18:17:25 +0200

    [+] add lfork instruction,

--- src/action/lfork.c
+++ src/action/lfork.c
@@ -9,5 +9,26 @@
 
 int lfork(corewar_t *war, robot_t *robot, processus_t *proc, int start_pc)
 {
+    int args = read_bytes_arena(war->arena, proc->new_pc, 2);
+
+    proc->new_pc = (proc->new_pc + IND_SIZE) % MEM_SIZE;
+    proc->todo[1] = args;
+    proc->todo[0] = 15;
+    proc->cycle = 999;
+    return SUCCESS_EXIT;
+}
+
+int do_lfork(corewar_t *war, robot_t *robot, processus_t *proc)
+{
+    processus_t *new = create_processus(robot->id);
+
+    if (!new)
+        return FAILURE;
+    new->carry = proc->carry;
+    new->pc = (proc->pc + proc->todo[1] + MEM_SIZE) % MEM_SIZE;
+    new->next = robot->processus;
+    robot->processus = new;
+    proc->pc = proc->new_pc;
+    reinit(proc);
     return SUCCESS_EXIT;
 }
