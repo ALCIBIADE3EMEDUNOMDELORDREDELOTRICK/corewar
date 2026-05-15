81ed5241a6e7015238b0a5bb17cc035e7f177f79
alcibiade Streit-Soumaila <alcibiade.streit-soumaila@epitech.eu>
2026-05-15 19:15:35 +0200

    [+] add zjmp function

--- src/action/zjmp.c
+++ src/action/zjmp.c
@@ -9,5 +9,23 @@
 
 int zjmp(corewar_t *war, robot_t *robot, processus_t *proc, int start_pc)
 {
+    int value = read_bytes_arena(war->arena, proc->new_pc, IND_SIZE);
+
+    proc->new_pc = (proc->new_pc + IND_SIZE) % MEM_SIZE;
+    if (proc->pc == 0) {
+        reinit(proc);
+        proc->pc = proc->new_pc;
+        return SUCCESS_EXIT;
+    }
+    proc->todo[0] = 9;
+    proc->todo[1] = value;
+    proc->cycle = 20;
+    return SUCCESS_EXIT;
+}
+
+int do_zjmp(corewar_t *war, robot_t *robot, processus_t *proc)
+{
+    proc->pc += proc->todo[1];
+    reinit(proc);
     return SUCCESS_EXIT;
 }
